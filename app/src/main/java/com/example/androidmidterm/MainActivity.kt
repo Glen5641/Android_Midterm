ce3e766234643e80ba1710980909044eec5d102f
ClayGlen <Glen5641@ou.edu>
2019-11-21 13:00:11 -0600

    Created Volley and Webview Random

--- app/src/main/java/com/example/androidmidterm/MainActivity.kt
+++ app/src/main/java/com/example/androidmidterm/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.example.androidmidterm
 
+/**
+ * Midterm Application Android
+ *
+ * STUDENT: Clayton Chase Glenn
+ * OUID:    113375641
+ * CLASS:   Software Engineering 1
+ * DATE:    11/21/2019
+ */
+
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.Response
-import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
-import com.example.androidmidterm.R
 import kotlinx.android.synthetic.main.activity_main.*
-import org.json.JSONArray
-import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,22 +26,26 @@ class MainActivity : AppCompatActivity() {
         // Volley uses internet permissions which needs to be enabled in the AndroidManifest.xml
         val queue = Volley.newRequestQueue(this)
 
-        // OU Calendar url
-        // TODO: change this to your url after you have endpoints
-        val url = "https://calendar.ou.edu/live/json/events"
+        // Url for Volley to get response from. Volley does not work locally with http requests.
+        // Only https requests which this issue was resolved with app engine being https.
+        val url = "https://software-engineering-1-250517.appspot.com/"
 
-        // OU calendar returns a Json Array, if your website returns a Json Object then use JsonObjectRequest
-        val stringRequest = JsonArrayRequest(Request.Method.GET, url, null,
-            Response.Listener<JSONArray> { response ->
-                val event = response[0] as JSONObject
-                // eventTitle is the id for the textbox in activity_main.xml
-                eventTitle.text = "Event title is : ${event.getString("title")}"
+        // Collect the Json object from the endpoint and display the value associated with "random" key
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+            Response.Listener { response ->
+                // Display the random number in eventTitle text box on screen
+                eventTitle.text = "Random Number: ${response.getString("random")}"
             },
             Response.ErrorListener { eventTitle.text = "That didn't work" })
 
-        // button is the id for the button in activity_main.xml
+        // Add the request to the queue if the button is clicked
         button.setOnClickListener {
-            queue.add(stringRequest)
+            queue.add(jsonObjectRequest)
+        }
+
+        // Add the webview to the queue if the button is clicked
+        button2.setOnClickListener{
+            webView.loadUrl(url)
         }
     }
 }
